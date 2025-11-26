fc6c243bb469af22aca837991cd37b7cdfaabfbe
IgorBiodev <biovet.santos@gmail.com>
2025-11-26 18:56:57 -0300

    iniciando JSON

--- class_bot.py
+++ class_bot.py
@@ -2,6 +2,7 @@ import telebot
 import database_manager as bc
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -13,10 +14,13 @@ class FocusBot:
 
         self.bot = telebot.TeleBot(chave)
         self.sessoes_ativas = {}
-    pass 
+
+    def debug(self, mensagem):
+        self.historico = json.dumps(self.sessoes_ativas, indent=4)
+        self.bot.reply_to(mensagem, self.historico)
+        
 
     def iniciar(self, mensagem):
-       # COMO ERA NO SCRIPT
         id_usuario = mensagem.chat.id
         texto_original = mensagem.text 
         tema = texto_original.replace('/iniciar','')
@@ -26,38 +30,62 @@ class FocusBot:
             return
 
         id_banco = bc.iniciar_sessao(id_usuario,tema)
+
+        dados_sessao = {
+            "id_banco": id_banco,
+            "tema": tema
+        }
     
-        self.sessoes_ativas[id_usuario] = id_banco
+        self.sessoes_ativas[id_usuario] = dados_sessao
         
         self.bot.reply_to(mensagem, f"🛡️ Sessão iniciada! Tema: {tema}\n(ID do Banco: {id_banco})")
-        pass
 
     def parar(self, mensagem):
-        # DESAFIO 2:
-        # Mesma coisa. Copie a lógica do 'parar' antigo e adapte.
-        pass
+        id_usuario = mensagem.chat.id
+
+        if id_usuario not in self.sessoes_ativas:
+            self.bot.reply_to(mensagem, "❌ Nenhuma sessão ativa para parar.")
+            return
+
+        
+        self.id_banco = self.sessoes_ativas[id_usuario]
+
+    
+        self.tempo = bc.encerrar_sessao(self.id_banco)
+
+        del self.sessoes_ativas[id_usuario]
+
+        self.bot.reply_to(mensagem, f"🛑 Foco Encerrado!\n⏱ Tempo Total: {self.tempo}")
 
     def deletar(self, mensagem):
-        # DESAFIO 3:
-        # Copie a lógica do 'deletar' antigo.
-        pass
+        id_usuario = mensagem.chat.id
+
+        try:
+            bc.del_sessao(id_usuario)
+            self.bot.reply_to(mensagem,'🗑️ Ultima sessão deletada com sucesso!')
+        except Exception as e:
+           self.bot.reply_to(mensagem, f'⚠️ Erro a deletar: {e}')
+
 
     def ligar(self):
-        # Aqui conectamos os comandos
-        # Eu vou te ajudar com um exemplo, você faz os outros.
-        
+        @self.bot.message_handler(commands=['debug'])
+        def handle_debug(msg):
+            self.debug(msg)
         @self.bot.message_handler(commands=['iniciar'])
-        def pre_iniciar(msg):
+        def handle_iniciar(msg):
             self.iniciar(msg)
-            
-        # AGORA É COM VOCÊ:
-        # Crie o registro para o comando '/parar' chamando self.parar
-        # Crie o registro para o comando '/deletar' chamando self.deletar
-        
-        print("Bot Online...")
+
+        @self.bot.message_handler(commands=['parar'])
+        def handle_parar(msg):
+            self.parar(msg)
+
+        @self.bot.message_handler(commands=['deletar'])
+        def handle_deletar(msg):
+            self.deletar(msg)
+
+        print("🤖 FocusBot System ONLINE...")
         self.bot.polling()
 
-# Execução
 if __name__ == "__main__":
     robo = FocusBot()
     robo.ligar()
